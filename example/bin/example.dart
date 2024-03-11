e8a3ab812491e6967029be72248a2f6289774b81
Mahdi <mkfard@outlook.com>
2024-03-11 14:28:12 +0330

    Update the example.

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -1,8 +1,28 @@
-import 'package:example/example.dart' as example;
+import 'dart:typed_data';
+
+import 'package:tg/tg.dart' as tg;
 
 void main(List<String> arguments) async {
-  final c = example.createClient(12345);
-  final result = await c.initConnection();
+  final buffer = <int>[];
 
-  print('Result: $result!');
+  final q = tg.InitConnection(
+    apiId: 123456,
+    deviceModel: "Windows",
+    appVersion: "1.0.0",
+    systemVersion: "10",
+    systemLangCode: "fa",
+    langCode: "fa",
+    langPack: "",
+    proxy: null,
+    params: null,
+    query: tg.HelpGetConfig(),
+  );
+
+  q.serialize(buffer);
+
+  final br = tg.BinaryReader(Uint8List.fromList(buffer));
+  final x = br.readObject<tg.InitConnection>();
+
+  print(buffer);
+  print(x);
 }
--- lib/src/client.dart
+++ lib/src/client.dart
@@ -20,21 +20,4 @@ class Client {
 
   Future<X> invokeWithLayer<X>(int layer, TlMethod<X> query) =>
       invoke(InvokeWithLayer<X>(layer: layer, query: query));
-
-  Future<ConfigBase> initConnection() {
-    final q = InitConnection(
-      apiId: apiId,
-      deviceModel: "Windows",
-      appVersion: "1.0.0",
-      systemVersion: "10",
-      systemLangCode: "fa",
-      langCode: "fa",
-      langPack: "",
-      proxy: null,
-      params: null,
-      query: HelpGetConfig(),
-    );
-
-    return invokeWithLayer<ConfigBase>(_layer, q);
-  }
 }
